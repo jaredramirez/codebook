bd6f26d9f0643288628f5ac8e0c4ea015d6c20f0
Jared Ramirez <jaredpramirez@proton.me>
2025-04-11 13:12:16 -0700

    Setup nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in {
+        packages.default = craneLib.buildPackage {
+          src = craneLib.cleanCargoSource ./.;
+
+          # Add extra inputs here or any other derivation settings
+          # doCheck = true;
+          # buildInputs = [];
+          # nativeBuildInputs = [];
+        };
+      });
+}
